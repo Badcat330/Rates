d60d4783d63413b4fd5047c16023adfccdb3c888
Alex <alextvrus@gmail.com>
2019-07-10 16:02:49 +0200

    Add spinner  for indicating progress of server requests.

--- Rates/SecondCurrencyViewController.swift
+++ Rates/SecondCurrencyViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
+
+
 class SecondCurrencyViewController: UIViewController {
 
   @IBOutlet weak var tableView: UITableView!
+  var vSpinner: UIView?
   let lines = try! String(contentsOfFile: Bundle.main.path(forResource: "Currency", ofType: "txt")!).split{$0.isNewline}
   var rate: Double?{
     didSet{
@@ -26,6 +29,7 @@ class SecondCurrencyViewController: UIViewController {
         rateViewController.loadViewIfNeeded()
         rateViewController.pairesOfCurrency.append(pairOfRates as! (firstRateRedustion: String, firstRateFullName: String, secondRateredustion:    String, secondRateFullName: String, rate: Double ))
         rateViewController.tableView.reloadData()
+        self.removeSpinner()
         navigationController?.popToViewController(rateViewController, animated: true)
       }
     }
@@ -41,6 +45,7 @@ class SecondCurrencyViewController: UIViewController {
   }
   
   func GetRate(firstCurrency: String, secondCurrency: String){
+      showSpinner(onView: self.view)
       let url = "https://api.exchangeratesapi.io/latest?base="
         + firstCurrency+"&symbols="
         + firstCurrency+"," + secondCurrency
@@ -88,5 +93,30 @@ extension SecondCurrencyViewController: UITableViewDelegate{
     nowSelection.append(selectedCell.reductionLabel[0].text!)
     nowSelection.append(selectedCell.fullNameLabel[0].text!)
     GetRate(firstCurrency: previousSelection[0], secondCurrency: nowSelection[0])
+  }
+}
+
+extension SecondCurrencyViewController {
+  // spiner indecator declaration
+  func showSpinner(onView : UIView) {
+    let spinnerView = UIView.init(frame: onView.bounds)
+    spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+    let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+    ai.startAnimating()
+    ai.center = spinnerView.center
+    
+    DispatchQueue.main.async {
+      spinnerView.addSubview(ai)
+      onView.addSubview(spinnerView)
+    }
+    
+    vSpinner = spinnerView
+  }
+  
+  func removeSpinner() {
+    DispatchQueue.main.async {
+      self.vSpinner?.removeFromSuperview()
+      self.vSpinner = nil
+    }
   }
 }
